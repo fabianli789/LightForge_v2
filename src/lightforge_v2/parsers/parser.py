9a59f1dfbe66642ccf4b730280a1e8f70240bdc1
Fabian Li <udskm@student.kit.edu>
2024-09-23 12:39:50 +0200

    testing run section

--- src/lightforge_v2/parsers/parser.py
+++ src/lightforge_v2/parsers/parser.py
@@ -14,6 +14,12 @@ from nomad.config import config
 from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.parsing.parser import MatchingParser
 
+#from nomad.datamodel.metainfo.simulation.run import Run                        # no import error but error at sec_run = archive.m_create(Run)
+#from nomad.datamodel.metainfo.simulation.calculation import Calculation
+
+#from runschema.run import Run, Program                                         # import error: no module named 'runschema'
+#from nomad_simulations.schema_packages.general import Program, Simulation      # import error: no module 'nomad_simulations'
+
 configuration = config.get_plugin_entry_point(
     'lightforge_v2.parsers:parser_entry_point'
 )
@@ -30,3 +36,9 @@ class NewParser(MatchingParser):
         logger.info('NewParser.parse', parameter=configuration.parameter)
 
         archive.workflow2 = Workflow(name='test')
+#        sec_program = archive.m_setdefault('run.program')
+#        sec_program.name = "Lightforge test test"
+    
+#        sec_run = archive.m_create(Run)
+#        sec_calc = Calculation()
+#        sec_calc.pressure = 5
